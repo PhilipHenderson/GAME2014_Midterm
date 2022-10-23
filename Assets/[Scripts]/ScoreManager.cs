41821c311b46027688db817d11de366763d28267
Philip Henderson <philip.henderson@hotmail.com>
2022-10-22 23:19:26 -0400

    Added Internal Documents to scripts, Deployed to Android

--- Assets/[Scripts]/ScoreManager.cs
+++ Assets/[Scripts]/ScoreManager.cs
@@ -1,3 +1,22 @@
+////////////////////////////////////////
+//
+// Created By: Philip Henderson
+// Student Number: 101137744
+//
+// Date Last Modified: October 22nd/2022
+//
+//
+// Source File: GAME2014-Midterm-101137744
+// Program: Video Game Programming
+//
+// Revision History: Changed the Core framework:
+//  - Player is placed on the left side of the screen
+//  - Player looking and shooting to the right
+//  - Enemies placed on the right side instead of the top
+//  - Enemies movement change from right-left to up-down
+//
+////////////////////////////////////////
+
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
